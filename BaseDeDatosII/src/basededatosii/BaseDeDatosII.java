a147176f0af399c6aa1866c2bd63188e54247a8f
user <user@DESKTOP-EOQAJOP>
2021-07-12 10:48:59 -0500

    Segundo Cambio

--- BaseDeDatosII/src/basededatosii/BaseDeDatosII.java
+++ BaseDeDatosII/src/basededatosii/BaseDeDatosII.java
@@ -15,7 +15,10 @@ public class BaseDeDatosII {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int x = 4;
+        int y = 8;
+       
+        System.out.println("la suma es:"+(x+y));
     }
     
 }
